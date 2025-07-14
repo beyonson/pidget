0cc4a30330dc650a48d921d4754472c0028a09c2
Garrett Marcinak <garrettmarcinak@gmail.com>
2025-07-14 12:37:11 -0400

    Format files

--- src/logger.c
+++ src/logger.c
@@ -1,20 +1,23 @@
+#include "logger.h"
 #include <stdio.h>
 #include <time.h>
-#include "logger.h"
 
 static LogLevel current_log_level = INFO;
 
-void set_log_level (LogLevel level)
+void
+set_log_level (LogLevel level)
 {
   current_log_level = level;
 }
 
-void log_message (LogLevel level, const char* text)
+void
+log_message (LogLevel level, const char *text)
 {
-  if (level < current_log_level) {
-    return;
-  }
+  if (level < current_log_level)
+    {
+      return;
+    }
 
-  const char* level_strings[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-  printf("[%s] %s\n", level_strings[level], text);
+  const char *level_strings[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+  printf ("[%s] %s\n", level_strings[level], text);
 }

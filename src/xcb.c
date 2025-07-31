058fdb4cd76fb9d510f8f80e71601676211ce71c
Garrett Marcinak <garrettmarcinak@gmail.com>
2025-07-31 10:02:38 -0400

    Refactor for readability

--- src/xcb.c
+++ src/xcb.c
@@ -11,9 +11,6 @@
 #include <xcb/xcb_icccm.h>
 #include <xcb/xcb_image.h>
 
-xcb_gcontext_t gc;
-xcb_pixmap_t backing_pixmap;
-
 xcb_visualtype_t *
 find_argb_visual (xcb_connection_t *conn, xcb_screen_t *screen)
 {
@@ -35,12 +32,9 @@ find_argb_visual (xcb_connection_t *conn, xcb_screen_t *screen)
   return NULL;
 }
 
-xcb_window_t
-xcb_init (xcb_connection_t *c, struct PixelBuffer png_buffer)
+int
+pidget_xcb_init (XcbObject *xcb_object, struct PixelBuffer *png_buffer)
 {
-  xcb_window_t win;
-  xcb_screen_t *screen;
-
   struct MotifHints motif_hints;
 
   motif_hints.flags = 2;
@@ -50,35 +44,38 @@ xcb_init (xcb_connection_t *c, struct PixelBuffer png_buffer)
   motif_hints.status = 0;
 
   /* Get the first screen */
-  screen = xcb_setup_roots_iterator (xcb_get_setup (c)).data;
+  xcb_object->screen
+      = xcb_setup_roots_iterator (xcb_get_setup (xcb_object->conn)).data;
 
   /* Request a window ID */
-  win = xcb_generate_id (c);
+  xcb_object->win = xcb_generate_id (xcb_object->conn);
 
   /* Set up atoms */
-  xcb_intern_atom_cookie_t motif_cookie
-      = xcb_intern_atom (c, 0, strlen ("_MOTIF_WM_HINTS"), "_MOTIF_WM_HINTS");
+  xcb_intern_atom_cookie_t motif_cookie = xcb_intern_atom (
+      xcb_object->conn, 0, strlen ("_MOTIF_WM_HINTS"), "_MOTIF_WM_HINTS");
   xcb_intern_atom_reply_t *motif_reply
-      = xcb_intern_atom_reply (c, motif_cookie, NULL);
+      = xcb_intern_atom_reply (xcb_object->conn, motif_cookie, NULL);
 
   /* Set window attributes */
   /* NOTE: Indices of win_events must align with mask according to xcb_cw_t */
   const uint32_t border_width[] = { 0 };
   const uint32_t border_color[] = { 0xFF09224A };
 
-  xcb_visualtype_t *argb_visual = find_argb_visual (c, screen);
+  /* TODO: allow non-ARGB visual */
+  xcb_visualtype_t *argb_visual
+      = find_argb_visual (xcb_object->conn, xcb_object->screen);
 
   if (!argb_visual)
     {
-      fprintf (stderr, "No ARGB visual found\n");
-      exit (1);
+      log_message (3, "No ARGB visual found\n");
+      return 1;
     }
 
-  xcb_colormap_t colormap = xcb_generate_id (c);
-  xcb_create_colormap (c, XCB_COLORMAP_ALLOC_NONE, colormap, screen->root,
-                       argb_visual->visual_id);
+  xcb_colormap_t colormap = xcb_generate_id (xcb_object->conn);
+  xcb_create_colormap (xcb_object->conn, XCB_COLORMAP_ALLOC_NONE, colormap,
+                       xcb_object->screen->root, argb_visual->visual_id);
 
-  win = xcb_generate_id (c);
+  xcb_object->win = xcb_generate_id (xcb_object->conn);
   uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_BORDER_PIXEL | XCB_CW_EVENT_MASK
                   | XCB_CW_COLORMAP;
   uint32_t valwin[4];
@@ -89,17 +86,20 @@ xcb_init (xcb_connection_t *c, struct PixelBuffer png_buffer)
   valwin[3] = colormap;
 
   /* Create the window */
-  xcb_create_window (c, 32, win, screen->root, 0, 0, png_buffer.width,
-                     png_buffer.height, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
+  xcb_create_window (xcb_object->conn, 32, xcb_object->win,
+                     xcb_object->screen->root, 0, 0, png_buffer->width,
+                     png_buffer->height, 0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
                      argb_visual->visual_id, mask, valwin);
 
-  xcb_change_window_attributes (c, win, XCB_CW_BORDER_PIXEL, border_color);
-  xcb_configure_window (c, win, XCB_CONFIG_WINDOW_BORDER_WIDTH, border_width);
+  xcb_change_window_attributes (xcb_object->conn, xcb_object->win,
+                                XCB_CW_BORDER_PIXEL, border_color);
+  xcb_configure_window (xcb_object->conn, xcb_object->win,
+                        XCB_CONFIG_WINDOW_BORDER_WIDTH, border_width);
 
   /* Set motif hints to remove window decorations */
-  xcb_change_property (c, XCB_PROP_MODE_REPLACE, win, motif_reply->atom,
-                       motif_reply->atom, 32, sizeof (motif_hints),
-                       &motif_hints);
+  xcb_change_property (xcb_object->conn, XCB_PROP_MODE_REPLACE,
+                       xcb_object->win, motif_reply->atom, motif_reply->atom,
+                       32, sizeof (motif_hints), &motif_hints);
   free (motif_reply);
 
   /* Set forced window size */
@@ -107,8 +107,15 @@ xcb_init (xcb_connection_t *c, struct PixelBuffer png_buffer)
 
   xcb_icccm_size_hints_set_max_size (&hints, WIDTH, HEIGHT);
   xcb_icccm_size_hints_set_min_size (&hints, WIDTH, HEIGHT);
-  xcb_icccm_set_wm_size_hints (c, win, XCB_ATOM_WM_NORMAL_HINTS, &hints);
+  xcb_icccm_set_wm_size_hints (xcb_object->conn, xcb_object->win,
+                               XCB_ATOM_WM_NORMAL_HINTS, &hints);
 
+  return 0;
+}
+
+int
+pidget_xcb_load_image (XcbObject *xcb_object, struct PixelBuffer png_buffer)
+{
   /* Create backing pixmap */
   png_bytep *rows = (png_bytep *)png_buffer.pixels;
   uint8_t frog_bytes[png_buffer.width * png_buffer.height * 4];
@@ -127,66 +134,41 @@ xcb_init (xcb_connection_t *c, struct PixelBuffer png_buffer)
   xcb_image_t *image;
 
   image = xcb_image_create_native (
-      c, png_buffer.width, png_buffer.height, XCB_IMAGE_FORMAT_Z_PIXMAP, 32,
-      (uint8_t *)frog_bytes, png_buffer.width * png_buffer.height * 4,
-      (uint8_t *)frog_bytes);
+      xcb_object->conn, png_buffer.width, png_buffer.height,
+      XCB_IMAGE_FORMAT_Z_PIXMAP, 32, (uint8_t *)frog_bytes,
+      png_buffer.width * png_buffer.height * 4, (uint8_t *)frog_bytes);
 
   if (!image)
     {
       log_message (3, "No image\n");
     }
 
-  uint32_t values[] = { screen->white_pixel, screen->white_pixel, 0 };
+  uint32_t values[] = { xcb_object->screen->white_pixel,
+                        xcb_object->screen->white_pixel, 0 };
 
-  backing_pixmap = xcb_generate_id (c);
+  xcb_object->backing_pixmap = xcb_generate_id (xcb_object->conn);
 
-  xcb_create_pixmap (c, 32, backing_pixmap, win, png_buffer.width,
-                     png_buffer.height);
+  xcb_create_pixmap (xcb_object->conn, 32, xcb_object->backing_pixmap,
+                     xcb_object->win, png_buffer.width, png_buffer.height);
 
-  gc = xcb_generate_id (c);
+  xcb_object->gc = xcb_generate_id (xcb_object->conn);
 
-  xcb_create_gc (c, gc, win,
+  xcb_create_gc (xcb_object->conn, xcb_object->gc, xcb_object->win,
                  XCB_GC_FOREGROUND | XCB_GC_BACKGROUND
                      | XCB_GC_GRAPHICS_EXPOSURES,
                  values);
 
-  log_message (0, "backing pixmap: %d\n", backing_pixmap);
+  log_message (0, "backing pixmap: %d\n", xcb_object->backing_pixmap);
 
-  xcb_image_put (c, backing_pixmap, gc, image, 0, 0, 0);
+  xcb_image_put (xcb_object->conn, xcb_object->backing_pixmap, xcb_object->gc,
+                 image, 0, 0, 0);
 
   /* Send image data to X server */
-  xcb_copy_area (c, backing_pixmap, win, gc, 0, 0, 0, 0, png_buffer.width,
+  xcb_copy_area (xcb_object->conn, xcb_object->backing_pixmap, xcb_object->win,
+                 xcb_object->gc, 0, 0, 0, 0, png_buffer.width,
                  png_buffer.height);
 
-  return win;
-}
-
-xcb_atom_t
-get_atom (xcb_connection_t *conn, const char *name)
-{
-  xcb_generic_error_t *e;
-  xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply (
-      conn, xcb_intern_atom (conn, 0, strlen (name), name), &e);
-
-  if (e != NULL)
-    {
-      log_message (3, "%s: failed to get atom", name);
-      free (e);
-      free (reply);
-
-      return (xcb_atom_t){ 0 };
-    }
-
-  xcb_atom_t ret = reply->atom;
-  log_message (0, "atom %s = 0x%08x", name, ret);
-
-  if (ret == XCB_ATOM_NONE)
-    log_message (3, "%s: no such atom", name);
-
-  assert (ret != XCB_ATOM_NONE);
-
-  free (reply);
-  return ret;
+  return 0;
 }
 
 void
@@ -201,14 +183,14 @@ move_window (xcb_connection_t *c, xcb_window_t win, int32_t x, int32_t y)
   if (!geom)
     {
       log_message (3, "Error: Failed to get geometry reply\n");
-      return;
+      goto error_geom;
     }
 
   tree = xcb_query_tree_reply (c, xcb_query_tree (c, win), NULL);
   if (!tree)
     {
       log_message (3, "Error: Failed to query tree\n");
-      return;
+      goto error_tree;
     }
 
   trans = xcb_translate_coordinates_reply (
@@ -217,7 +199,7 @@ move_window (xcb_connection_t *c, xcb_window_t win, int32_t x, int32_t y)
   if (!trans)
     {
       log_message (3, "Error: Failed to get coordinates reply\n");
-      return;
+      goto error_trans;
     }
 
   uint32_t mask = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
@@ -229,12 +211,17 @@ move_window (xcb_connection_t *c, xcb_window_t win, int32_t x, int32_t y)
 
   /* the translated coordinates are in trans->dst_x and trans->dst_y */
   free (trans);
+
+error_trans:
   free (tree);
+error_tree:
   free (geom);
+error_geom:
+  return;
 }
 
 void
-handle_event (xcb_connection_t *c, xcb_window_t win, xcb_generic_event_t *e)
+handle_event (XcbObject *xcb_object, xcb_generic_event_t *e)
 {
   switch (e->response_type)
     {
@@ -242,7 +229,7 @@ handle_event (xcb_connection_t *c, xcb_window_t win, xcb_generic_event_t *e)
       {
         xcb_generic_error_t *err = (xcb_generic_error_t *)e;
         xcb_errors_context_t *err_ctx;
-        xcb_errors_context_new (c, &err_ctx);
+        xcb_errors_context_new (xcb_object->conn, &err_ctx);
         const char *major, *minor, *extension, *error;
         major = xcb_errors_get_name_for_major_code (err_ctx, err->major_code);
         minor = xcb_errors_get_name_for_minor_code (err_ctx, err->major_code,
@@ -264,16 +251,18 @@ handle_event (xcb_connection_t *c, xcb_window_t win, xcb_generic_event_t *e)
       {
         /* Add handling code */
         xcb_key_release_event_t *ev = (xcb_key_release_event_t *)e;
-        move_window (c, win, ev->root_x, ev->root_y);
-        xcb_flush (c);
+        move_window (xcb_object->conn, xcb_object->win, ev->root_x,
+                     ev->root_y);
+        xcb_flush (xcb_object->conn);
         break;
       }
     case XCB_EXPOSE:
       {
         xcb_expose_event_t *ev = (xcb_expose_event_t *)e;
-        xcb_copy_area (c, backing_pixmap, win, gc, 0, 0, 0, 0, ev->width,
+        xcb_copy_area (xcb_object->conn, xcb_object->backing_pixmap,
+                       xcb_object->win, xcb_object->gc, 0, 0, 0, 0, ev->width,
                        ev->height);
-        xcb_flush (c);
+        xcb_flush (xcb_object->conn);
         break;
       }
     default:

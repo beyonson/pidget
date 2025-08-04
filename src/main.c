2b9bb05299835094eaf6dab865f0360de659610a
Garrett Marcinak <garrettmarcinak@gmail.com>
2025-08-04 15:05:31 +0100

    Add timer based movement

--- src/main.c
+++ src/main.c
@@ -2,16 +2,46 @@
 #include "logger.h"
 #include "xcb.h"
 #include <assert.h>
+#include <ev.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <xcb/xcb.h>
+
+ev_timer timeout_watcher;
+ev_io xcb_watcher;
+
+struct PixelBuffer png_buffer;
+struct XcbObject xcb_object;
+
+static void
+xcb_event_cb (EV_P_ ev_io *w, int revents)
+{
+  xcb_generic_event_t *e;
+  while ((e = xcb_poll_for_event (xcb_object.conn)))
+    {
+      if (e != NULL)
+        {
+          handle_event (&xcb_object, e);
+          free (e);
+        }
+      else
+        {
+          log_message (0, "No event received\n");
+        }
+    }
+}
+
+static void
+timeout_cb (EV_P_ ev_timer *w, int revents)
+{
+  pidget_move_random (&xcb_object);
+}
 
 int
 main ()
 {
   int screen_num;
-  struct PixelBuffer png_buffer;
-  struct XcbObject xcb_object;
 
   set_log_level (0);
 
@@ -29,13 +59,16 @@ main ()
   xcb_flush (xcb_object.conn);
 
   /* Event loop */
-  /* TODO: Use libev for event loop */
-  xcb_generic_event_t *e;
-  while ((e = xcb_wait_for_event (xcb_object.conn)))
-    {
-      handle_event (&xcb_object, e);
-      free (e);
-    }
+  struct ev_loop *loop = EV_DEFAULT;
+  ev_timer_init (&timeout_watcher, timeout_cb, 1.0,
+                 2.0);
+  ev_timer_start (loop, &timeout_watcher);
+
+  ev_io_init (&xcb_watcher, xcb_event_cb,
+              xcb_get_file_descriptor (xcb_object.conn), EV_READ);
+  ev_io_start (loop, &xcb_watcher);
+
+  ev_run (loop, 0);
 
   xcb_disconnect (xcb_object.conn);
 
